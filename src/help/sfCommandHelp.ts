a730434bf972b4675ea221a2f18d2b0f28d82ab9
Peter Hale <peter.hale@salesforce.com>
2021-09-14 13:29:51 -0600

    fix: update telemetry

--- src/help/sfCommandHelp.ts
+++ src/help/sfCommandHelp.ts
@@ -5,7 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import { CommandHelp, HelpSection, HelpSectionRenderer, Interfaces } from '@oclif/core';
-import { SfCommandInterface } from '@sf/cmd';
+import { SfCommandInterface } from '@salesforce/command';
 type SectionType = { header: string; generate: HelpSectionRenderer };
 
 export class SfCommandHelp extends CommandHelp {
