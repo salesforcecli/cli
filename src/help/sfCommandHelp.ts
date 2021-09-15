f917ffd5fab0432128dec8f7291e8ef1f53489a4
Peter Hale <peter.hale@salesforce.com>
2021-09-15 14:02:16 -0600

    fix: update cli with current plugin versions
    @W-9708749@

--- src/help/sfCommandHelp.ts
+++ src/help/sfCommandHelp.ts
@@ -5,7 +5,7 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import { CommandHelp, HelpSection, HelpSectionRenderer, Interfaces } from '@oclif/core';
-import { SfCommandInterface } from '@salesforce/command';
+import { SfCommandInterface } from '@salesforce/sf-plugins-core';
 type SectionType = { header: string; generate: HelpSectionRenderer };
 
 export class SfCommandHelp extends CommandHelp {
