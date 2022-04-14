9079ea896eccfebd5cb36945fbd3c339fbb34ca1
Mike Donnalley <mdonnalley@salesforce.com>
2022-04-14 08:13:16 -0700

    Automatically highlight command names in help (#212)
    * chore: support formatting in messages
    
    * fix: use proper symbol for bold
    
    * chore: bump oclif/core
    
    * feat: automatically format command names in help

--- src/help/sfHelp.ts
+++ src/help/sfHelp.ts
@@ -5,15 +5,27 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import { CommandHelp, Help, Interfaces } from '@oclif/core';
+import * as chalk from 'chalk';
 import { SfCommandHelp } from './sfCommandHelp';
 
 export default class SfHelp extends Help {
   protected CommandHelpClass: typeof CommandHelp = SfCommandHelp;
   protected commandHelpClass: SfCommandHelp;
   private showShortHelp = false;
+  private commands: string[] = [];
+  private subCommands: Record<string, string[]> = {};
 
   public async showHelp(argv: string[]): Promise<void> {
     this.showShortHelp = argv.includes('-h');
+    this.commands = this.config.commandIDs.map(
+      (c) => `${this.config.bin} ${c.replace(/:/g, this.config.topicSeparator)}`
+    );
+    for (const cmd of this.commands) {
+      this.subCommands[cmd] = this.commands
+        .filter((c) => c.startsWith(cmd) && c !== cmd)
+        .map((c) => `${c.replace(cmd, '')}`);
+    }
+
     return await super.showHelp(argv);
   }
 
@@ -21,5 +33,22 @@ export default class SfHelp extends Help {
     this.commandHelpClass = super.getCommandHelpClass(command) as SfCommandHelp;
     this.commandHelpClass.showShortHelp = this.showShortHelp;
     return this.commandHelpClass;
+  }
+
+  protected log(...args: string[]): void {
+    const formatted = args.map((arg) => {
+      for (const cmd of this.commands) {
+        const regex =
+          this.subCommands[cmd].length > 0
+            ? new RegExp(`(?<!\\$ )${cmd}(?!${this.subCommands[cmd].join('|')})`, 'g')
+            : new RegExp(`(?<!\\$ )${cmd}`, 'g');
+
+        arg = arg.replace(regex, chalk.dim(cmd));
+      }
+
+      return arg;
+    });
+
+    super.log(...formatted);
   }
 }
