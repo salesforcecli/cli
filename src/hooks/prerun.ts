f9bc07537e27cc46f6e1ae9a7a8d21c4104ea82e
Mike Donnalley <mdonnalley@salesforce.com>
2023-09-07 11:04:23 -0600

    fix: show warning only for plugin to which the command belongs

--- src/hooks/prerun.ts
+++ src/hooks/prerun.ts
@@ -8,23 +8,21 @@
 import { Hook, ux } from '@oclif/core';
 
 // eslint-disable-next-line @typescript-eslint/require-await
-const hook: Hook.Prerun = async function ({ config }) {
+const hook: Hook.Prerun = async function ({ Command, config }) {
+  const { plugin } = Command;
+  if (!plugin) return;
+  if (plugin.type === 'link') return;
+
   const jitPlugins = config.pjson.oclif.jitPlugins ?? {};
   const deps = config.pjson.dependencies ?? {};
-  for (const plugin of config.getPluginsList()) {
-    // Skip the root plugin
-    if (plugin.root === config.root) continue;
-    // Skip user plugins that are not in the jitPlugins configuration
-    if (plugin.type === 'user' && !jitPlugins[plugin.name]) continue;
-    // Skip linked plugins
-    if (plugin.type === 'link') continue;
 
-    const specifiedVersion = jitPlugins[plugin.name] ?? deps[plugin.name];
-    if (plugin.version !== specifiedVersion) {
-      ux.warn(
-        `Plugin ${plugin.name} (${plugin.version}) differs from the version specified by ${config.bin} (${specifiedVersion})`
-      );
-    }
+  const specifiedVersion = jitPlugins[plugin.name] ?? deps[plugin.name];
+  if (!specifiedVersion) return;
+
+  if (plugin.version !== specifiedVersion) {
+    ux.warn(
+      `Plugin ${plugin.name} (${plugin.version}) differs from the version specified by ${config.bin} (${specifiedVersion})`
+    );
   }
 };
 
