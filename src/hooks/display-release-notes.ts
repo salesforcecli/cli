6f85ef86b83a8bc7b052ada0efeb23eb66353f4d
Mike Donnalley <mdonnalley@salesforce.com>
2023-09-19 10:02:20 -0600

    refactor: display-release-notes hook

--- src/hooks/display-release-notes.ts
+++ src/hooks/display-release-notes.ts
@@ -4,52 +4,19 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
-import { join } from 'path';
-import { spawn } from 'child_process';
 import { Hook, ux } from '@oclif/core';
 
-const logError = (msg: Error): void => {
-  ux.log('NOTE: This error can be ignored in CI and may be silenced in the future');
-  ux.log('- Set the SF_HIDE_RELEASE_NOTES env var to "true" to skip this script\n');
-  ux.log(msg.toString());
+export const hook: Hook<'update'> = async function ({ config }) {
+  if (process.env.SF_HIDE_RELEASE_NOTES === 'true') return;
+
+  try {
+    return await config.runCommand('whatsnew', ['--hook']);
+  } catch (err) {
+    const error = err as Error;
+    ux.log('NOTE: This error can be ignored in CI and may be silenced in the future');
+    ux.log('- Set the SF_HIDE_RELEASE_NOTES env var to "true" to skip this script\n');
+    ux.log(error.message);
+  }
 };
-
-/*
- * NOTE: Please read "Notes about the hook scripts" in this PR before making changes:
- * https://github.com/salesforcecli/sfdx-cli/pull/407
- */
-
-const hook: Hook.Update = async (): Promise<void> =>
-  new Promise((resolve) => {
-    if (process.env.SF_HIDE_RELEASE_NOTES === 'true') {
-      resolve();
-    }
-
-    const executable = process.platform === 'win32' ? 'run.cmd' : 'run';
-    const cmd = spawn(join(__dirname, '..', '..', 'bin', executable), ['whatsnew', '--hook'], {
-      stdio: ['ignore', 'inherit', 'pipe'],
-      timeout: 10000,
-    });
-
-    cmd.stderr.on('data', (error: Error) => {
-      logError(error);
-      resolve();
-    });
-
-    cmd.on('error', (error: Error) => {
-      logError(error);
-      resolve();
-    });
-
-    // 'exit' fires whether or not the stream are finished
-    cmd.on('exit', () => {
-      resolve();
-    });
-
-    cmd.on('close', () => {
-      resolve();
-    });
-  });
 
 export default hook;
