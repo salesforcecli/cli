bbff4dbb9e5b461bdd3f2f2cb047eab81221c519
Mike Donnalley <mdonnalley@salesforce.com>
2024-04-23 15:19:57 -0600

    chore: clean up

--- bin/dev.js
+++ bin/dev.js
@@ -1,18 +1,9 @@
-#!/usr/bin/env tsx
+#!/usr/bin/env ts-node
 
 async function main() {
   const oclif = await import('@oclif/core');
   oclif.settings.performanceEnabled = true;
-  oclif.settings.debug = true;
-  const { logger } = await import('../dist/sf-logger.js');
-  await oclif.execute({
-    development: true,
-    dir: import.meta.url,
-    loadOptions: {
-      root: import.meta.dirname,
-      // logger,
-    },
-  });
+  await oclif.execute({ development: true, dir: import.meta.url });
 }
 
 await main();
