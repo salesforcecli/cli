8820139b1c4d96651bfdc6eb3c0dee8526b6e792
Mike Donnalley <mdonnalley@salesforce.com>
2025-06-03 13:37:37 -0600

    test: use regex

--- test/hooks/prerun.test.ts
+++ test/hooks/prerun.test.ts
@@ -10,11 +10,26 @@ import { expect } from 'chai';
 import { runHook } from '@oclif/test';
 import { Config } from '@oclif/core/config';
 
+function sanitize(stderr: string): string {
+  return stderr
+    .replaceAll(' ›   ', ' ')
+    .split('\n')
+    .map((s) => s.trim())
+    .join(' ');
+}
+
 describe('Prerun Hook', () => {
-  const WARNING_TEXT = 'is older than the version specified by';
+  const WARNING_REGEX =
+    /Warning: Plugin ([^ ]+) \((\d+\.\d+\.\d+(?:-[^)]*)?)\) is older than the version specified by ([^ ]+) \((\d+\.\d+\.\d+(?:-[^)]*)?)\)/;
+  let config: Config;
+
+  before(async () => {
+    config = await Config.load({ root: dirname(fileURLToPath(import.meta.url)) });
+  });
+
   it('should do nothing when --json is present', async () => {
     const { stderr } = await runHook('prerun', { argv: ['--json'] });
-    expect(stderr).to.not.include(WARNING_TEXT);
+    expect(sanitize(stderr)).to.not.match(WARNING_REGEX);
   });
 
   it('should do nothing if plugin type is "link"', async () => {
@@ -22,7 +37,7 @@ describe('Prerun Hook', () => {
       type: 'link',
     };
     const { stderr } = await runHook('prerun', { Command: command, argv: [] });
-    expect(stderr).to.not.include(WARNING_TEXT);
+    expect(sanitize(stderr)).to.not.match(WARNING_REGEX);
   });
 
   it('should do nothing if plugin is not JIT or core', async () => {
@@ -34,119 +49,114 @@ describe('Prerun Hook', () => {
       },
     };
     const { stderr } = await runHook('prerun', { Command: command, argv: [] });
-    expect(stderr).to.not.include(WARNING_TEXT);
+    expect(sanitize(stderr)).to.not.match(WARNING_REGEX);
   });
 
   it('should warn if plugin version is less than specified version', async () => {
     const command = {
       plugin: {
-        type: 'jit',
+        type: 'core',
         name: '@salesforce/plugin-packaging',
         version: '0.0.1',
       },
     };
     const { stderr } = await runHook('prerun', { Command: command, argv: [] });
-    expect(stderr).to.include(WARNING_TEXT);
+    expect(sanitize(stderr)).to.match(WARNING_REGEX);
   });
 
   it('should not warn if plugin version is greater than specified version', async () => {
     const command = {
       plugin: {
-        type: 'jit',
+        type: 'core',
         name: '@salesforce/plugin-packaging',
         version: '10000.0.0',
       },
     };
     const { stderr } = await runHook('prerun', { Command: command, argv: [] });
-    expect(stderr).to.not.include(WARNING_TEXT);
+    expect(sanitize(stderr)).to.not.match(WARNING_REGEX);
   });
 
   it('should not warn if plugin version is equal to specified version', async () => {
     const command = {
       plugin: {
-        type: 'jit',
+        type: 'core',
         name: '@salesforce/plugin-packaging',
         version: '1.0.0',
       },
     };
-    const config = await Config.load({ root: dirname(fileURLToPath(import.meta.url)) });
 
     config.pjson.dependencies = {
       ...config.pjson.dependencies,
       '@salesforce/plugin-packaging': '1.0.0',
     };
     const { stderr } = await runHook('prerun', { Command: command, argv: [] }, config);
-    expect(stderr).to.not.include(WARNING_TEXT);
+    expect(sanitize(stderr)).to.not.match(WARNING_REGEX);
   });
 
   it('should warn if plugin version is an older prerelease', async () => {
     const command = {
       plugin: {
-        type: 'jit',
+        type: 'core',
         name: '@salesforce/plugin-packaging',
         version: '1.0.0-beta.1',
       },
     };
-    const config = await Config.load({ root: dirname(fileURLToPath(import.meta.url)) });
 
     config.pjson.dependencies = {
       ...config.pjson.dependencies,
       '@salesforce/plugin-packaging': '1.0.0-beta.2',
     };
     const { stderr } = await runHook('prerun', { Command: command, argv: [] }, config);
-    expect(stderr).to.include(WARNING_TEXT);
+    expect(sanitize(stderr)).to.match(WARNING_REGEX);
   });
 
   it('should not warn if plugin version is a newer prerelease', async () => {
     const command = {
       plugin: {
-        type: 'jit',
+        type: 'core',
         name: '@salesforce/plugin-packaging',
         version: '1.0.0-beta.2',
       },
     };
-    const config = await Config.load({ root: dirname(fileURLToPath(import.meta.url)) });
 
     config.pjson.dependencies = {
       ...config.pjson.dependencies,
       '@salesforce/plugin-packaging': '1.0.0-beta.1',
     };
     const { stderr } = await runHook('prerun', { Command: command, argv: [] }, config);
-    expect(stderr).to.not.include(WARNING_TEXT);
+    expect(sanitize(stderr)).to.not.match(WARNING_REGEX);
   });
 
   it('should not warn if plugin version is a newer prerelease and specified version is an older stable release', async () => {
     const command = {
       plugin: {
-        type: 'jit',
+        type: 'core',
         name: '@salesforce/plugin-packaging',
         version: '1.0.0-beta.1',
       },
     };
-    const config = await Config.load({ root: dirname(fileURLToPath(import.meta.url)) });
 
     config.pjson.dependencies = {
       ...config.pjson.dependencies,
       '@salesforce/plugin-packaging': '1.0.0',
     };
     const { stderr } = await runHook('prerun', { Command: command, argv: [] }, config);
-    expect(stderr).to.not.include(WARNING_TEXT);
+    expect(sanitize(stderr)).to.not.match(WARNING_REGEX);
   });
 
   it('should warn if plugin version is an older prerelease and specified version is a newer stable release', async () => {
     const command = {
       plugin: {
-        type: 'jit',
+        type: 'core',
         name: '@salesforce/plugin-packaging',
         version: '1.0.0-beta.1',
       },
     };
-    const config = await Config.load({ root: dirname(fileURLToPath(import.meta.url)) });
     config.pjson.dependencies = {
       ...config.pjson.dependencies,
       '@salesforce/plugin-packaging': '1.0.1',
     };
     const { stderr } = await runHook('prerun', { Command: command, argv: [] }, config);
-    expect(stderr).to.include(WARNING_TEXT);
+    expect(sanitize(stderr)).to.match(WARNING_REGEX);
   });
 });
